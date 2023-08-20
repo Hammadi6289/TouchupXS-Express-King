cea43946330a7787b8f10741c7cdb8fc9864be89
Hammad Chohan <Hammad6289@email.com>
2023-08-20 21:14:24 +0500

    Create Place Order Screen

--- README.md
+++ README.md
@@ -115,3 +115,10 @@
     1. create input forms
     2. handle submit
     3. create backend api for signup in the userRoutes
+26. Implement Select Payment Method Screen
+    1. create input forms
+    2. handle submit
+27. Create Place Order Screen
+    1. Preview cart items and payment etc
+    2. handle place order action
+    3. create order create api
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,6 +16,8 @@ import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SignupScreen from './screens/SignupScreen';
+import PaymentMethodScreen from './screens/PaymentMethodScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -25,6 +27,7 @@ function App() {
     ctxDispatch({ type: 'USER_SIGNOUT' });
     localStorage.removeItem('userInfo');
     localStorage.removeItem('shippingAddress');
+    localStorage.removeItem('paymentMethod');
   };
   return (
     <BrowserRouter>
@@ -78,10 +81,12 @@ function App() {
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
               <Route path="/signup" element={<SignupScreen />} />
+              <Route path="/placeorder" element={<PlaceOrderScreen />} />
               <Route
                 path="/shipping"
                 element={<ShippingAddressScreen />}
               ></Route>
+              <Route path="/payment" element={<PaymentMethodScreen />}></Route>
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>

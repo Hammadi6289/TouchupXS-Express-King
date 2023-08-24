56c8927506dbb383276b0c36e8ab24d861aab61b
Hammad Chohan <Hammad6289@email.com>
2023-08-24 23:09:30 +0500

    Create Order Screen

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -23,4 +23,17 @@ orderRouter.post(
     res.status(201).send({ message: 'New Order Created', order });
   })
 );
+orderRouter.get(
+  '/:id',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id); // findById is a Mongoose function
+    if (order) {
+      res.send(order);
+    } else {
+      res.status(404).send({ message: 'Order Not Found' });
+    }
+  })
+);
+
 export default orderRouter;

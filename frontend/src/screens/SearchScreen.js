b8d113f2391e471d4eb05916e07535b540d3da49
Hammad Chohan <Hammad6289@email.com>
2023-10-09 22:41:58 +0500

    Create Admin Menu

--- frontend/src/screens/SearchScreen.js
+++ frontend/src/screens/SearchScreen.js
@@ -36,16 +36,16 @@ const reducer = (state, action) => {
 
 const prices = [
   {
-    name: '$1 to $50',
-    value: '1-50',
+    name: '$1 to $20',
+    value: '1-20',
   },
   {
-    name: '$51 to $200',
-    value: '51-200',
+    name: '$21 to $58',
+    value: '21-58',
   },
   {
-    name: '$201 to $1000',
-    value: '201-1000',
+    name: '$59 to $1000',
+    value: '59-1000',
   },
 ];
 
